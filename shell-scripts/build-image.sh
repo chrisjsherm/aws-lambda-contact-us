c09854cf376dc04bb0cf2969f26e2bb846051a11
Christopher Sherman <chris@shermandigital.com>
2022-10-25 10:45:40 -0400

    Assume build-image.sh run from the repo root

--- shell-scripts/build-image.sh
+++ shell-scripts/build-image.sh
@@ -5,5 +5,5 @@ IMAGE_TAG=latest
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
-docker build -t ${IMAGE_REPO_NAME}:${IMAGE_TAG} ../ --progress=tty
+docker build -t ${IMAGE_REPO_NAME}:${IMAGE_TAG} . --progress=tty
 docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}
